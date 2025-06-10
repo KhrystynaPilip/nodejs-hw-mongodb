0c5495215816e5130673b7f3e2edbf89e93f1eda
Khrystyna Pilip <tina.hristi@gmail.com>
2025-06-10 13:14:22 +0200

    Rout fix

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -35,7 +35,7 @@ router.post('/logout', ctrlWrapper(logoutUserController));
 router.post('/refresh', ctrlWrapper(refreshUsersSessionController));
 
 router.post(
-  '/sent-reset-email',
+  '/send-reset-email',
   validateBody(requestResetEmailSchema),
   ctrlWrapper(requestResetEmailController),
 );
